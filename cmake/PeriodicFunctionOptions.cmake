021c86c1f21ee0c33cd9e7225041bea4ceb37a54
Paul T <developer.paul.123@gmail.com>
2020-08-19 11:33:24 -0400

    Remove Test Coverage Support (for now)

--- cmake/PeriodicFunctionOptions.cmake
+++ cmake/PeriodicFunctionOptions.cmake
@@ -1,5 +1,4 @@
 option(BUILD_TESTS "Turn on to build unit tests." ON)
 
 include(CMakeDependentOption)
-cmake_dependent_option(ENABLE_TEST_COVERAGE "Enable test coverage" OFF "BUILD_TESTS" OFF)
 cmake_dependent_option(TEST_INSTALLED_VERSION "Test the version found by find_package" OFF "BUILD_TESTS" OFF)
